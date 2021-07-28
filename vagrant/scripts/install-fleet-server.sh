afe0f9bd39eebabd57b23dc7f446af5ae94b0ba5
webhead404 <kvvincentvalentine@gmail.com>
2021-07-28 11:23:18 -0500

    Correct script

---
+++ vagrant/scripts/install-fleet-server.sh
@@ -0,0 +1,46 @@
+
+#!/bin/bash -eu
+
+set -o pipefail
+
+STACK_VER="${ELASTIC_STACK_VERSION:-7.13.4}"
+KIBANA_URL="${KIBANA_URL:-http://127.0.0.1:5601}"
+KIBANA_AUTH="${KIBANA_AUTH:-vagrant:vagrant}"
+ELASTICSEARCH_URL="${ELASTICSEARCH:-http://127.0.0.1:9200}"
+
+AGENT_URL="https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-${STACK_VER}-linux-x86_64.tar.gz"
+
+function install_jq() {
+    if ! command -v jq; then
+        sudo yum install -y jq
+    fi
+}
+
+function download_and_install_agent () {
+    declare -a AUTH=()
+    declare -a HEADERS=(
+        "-H" "Content-Type: application/json" 
+        "-H" "kbn-xsrf: fleet"
+    )
+
+    if [ -n "${KIBANA_AUTH}" ]; then
+        AUTH=("-u" "${KIBANA_AUTH}")
+    fi
+
+    response=$(curl --silent -XPOST "${AUTH[@]}" "${HEADERS[@]}" "${KIBANA_URL}/api/fleet/service-tokens" | jq -r '.value')
+    ENROLLMENT_TOKEN=$(echo -n "${response}")
+
+    #echo -n "${ENROLLMENT_TOKEN}"
+
+    cd "$(mktemp -d)"
+    curl --silent -LJ "${AGENT_URL}" | tar xzf -
+    cd "$(basename "$(basename "${AGENT_URL}")" .tar.gz)"
+    sudo ./elastic-agent install -f --fleet-server-es="${ELASTICSEARCH_URL}" --fleet-server-service-token="${ENROLLMENT_TOKEN}"
+    
+    # Cleanup temporary directory
+    cd ..
+    rm -rf "$(pwd)"
+}
+
+install_jq
+download_and_install_agent
